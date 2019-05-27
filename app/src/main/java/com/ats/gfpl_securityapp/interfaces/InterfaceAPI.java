7a2650a4b4e84e82672f6420f2a12333c71fde43
Aaryatech <code@atsinfosoft.com>
2019-05-27 14:39:58 +0530

    //Anmol

---
+++ app/src/main/java/com/ats/gfpl_securityapp/interfaces/InterfaceAPI.java
@@ -0,0 +1,11 @@
+package com.ats.gfpl_securityapp.interfaces;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface InterfaceAPI {
+
+
+}
